dc59ae901ddbeca0a6a4a46b83a18243bbf4799e
vvvasavii <vasavidwivedi180@gmail.com>
2026-04-08 23:17:41 +0530

    db commits

---
+++ backend/server.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const cors = require("cors");
+const pool = require("./config/db");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (_req, res) => {
+  res.send("API running");
+});
+
+pool.connect()
+  .then(() => console.log("DB connected"))
+  .catch(err => console.log(err));
+
+const PORT = 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
